0fe6052d9ef2104517c43137af90f4baf2869eef
Marcelo de Souza Vieira <marcelosouzav@gmail.com>
2021-12-30 23:50:58 -0300

    alterado dependencia do google

--- src/main/java/org/tradedigital/elasticcapturemethoddata/annotation/CaptureMethodDataAspect.java
+++ src/main/java/org/tradedigital/elasticcapturemethoddata/annotation/CaptureMethodDataAspect.java
@@ -1,7 +1,5 @@
 package org.tradedigital.elasticcapturemethoddata.annotation;
 
-
-
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
